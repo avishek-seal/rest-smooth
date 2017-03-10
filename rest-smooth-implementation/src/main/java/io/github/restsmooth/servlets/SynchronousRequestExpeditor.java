27477d8d096d867f95b62e8401c27b16c383a9f7
Avishek Seal <seal.avishek@yahoo.com>
2017-03-10 18:27:56 +0530

    - Implementation -- Asynchronous & Synchronous Request Expeditor servlets added

---
+++ rest-smooth-implementation/src/main/java/io/github/restsmooth/servlets/SynchronousRequestExpeditor.java
@@ -0,0 +1,142 @@
+package io.github.restsmooth.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * Servlet implementation class AsynchronousRequestExpeditor
+ * 
+ * @author Avishek Seal
+ * @since Mar 10, 2017
+ *
+ */
+public final class SynchronousRequestExpeditor extends HttpServlet {
+
+	private static final long serialVersionUID = 286079290995859914L;
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 * @param config
+	 * @throws ServletException
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.GenericServlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doPut(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doHead(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doOptions(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * 
+	 * @author Avishek Seal
+	 * @since Mar 10, 2017
+	 * @return @see javax.servlet.http.HttpServlet#doTrace(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+}
